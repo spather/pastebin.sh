e9bca65894047f54e84bdcb2d4583c473be2c35f
Shyam Pather <shyam.pather@gmail.com>
2019-11-02 22:44:16 -0700

    Initial checkin

---
+++ pastebin.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set the pastebin username (change if not $USER)
+API_USERNAME=$USER
+
+# Get the pastebin API devkey and password from keychain.
+# Assumes these were previously saved in the keychain with
+# the following commands:
+# security add-generic-password -a {pastebin username} -s pastebin-api-devkey -w {pastebin api dev key}
+# security add-generic-password -a {pastebin username} -s pastebin -w {pastebin password}
+API_DEV_KEY=$(security find-generic-password -a $API_USERNAME -s pastebin-api-devkey -w)
+API_PASSWORD=$(security find-generic-password -a $API_USERNAME -s pastebin -w)
+
+# Obtain a user key by logging in (this will be used in the next request to create a paste owned by
+# the logged-in user.
+api_user_key=$(\
+    curl -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
+         -d "api_user_name=$API_USERNAME&api_user_password=$API_PASSWORD&api_dev_key=$API_DEV_KEY" \
+         'http://pastebin.com/api/api_login.php' \
+)
+
+# Read input from stdin
+input=$(cat)
+
+# Create the paste
+url=$(\
+      curl -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" \
+	   -d "api_option=paste&api_paste_code=$input&api_dev_key=$API_DEV_KEY&api_user_key=$api_user_key" \
+	   'http://pastebin.com/api/api_post.php' \
+)
+
+echo $url
